785cc2f4d2477f7f2c788497d8a4b5ec6c3ec504
rbulle <rbulle@tutanota.com>
2025-08-31 15:37:47 +0200

    Update test_compute_meshtags and add quad mesh test data.

--- phiFEM/tests/test_compute_meshtags.py
+++ phiFEM/tests/test_compute_meshtags.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 from phiFEM.phifem.mesh_scripts import _tag_cells, _tag_facets
 import os
-from test_outward_normal import create_disk # type: ignore
+from tests_data.utils import create_disk, create_square, create_square_quad # type: ignore
 
 from tags_plot.plot import plot_mesh_tags
 import matplotlib.pyplot as plt
@@ -14,29 +14,41 @@ import matplotlib.pyplot as plt
 """
 Data_n° = ("Data name", "mesh name", levelset object, "cells benchmark name", "facets benchmark name")
 """
-data_1 = ("Circle radius 1", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, "celltags_1", "facettags_1", -1)
-data_2 = ("Circle radius 1", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, "celltags_1", "facettags_1", 1)
-data_3 = ("Circle radius 1", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, "celltags_1", "facettags_1", 2)
-data_4 = ("Circle radius 1", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, "celltags_1", "facettags_1", 3)
-data_5 = ("Boundary touching", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, "celltags_2", "facettags_2", -1)
-data_6 = ("Boundary touching", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, "celltags_2", "facettags_2", 1)
-data_7 = ("Boundary touching", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, "celltags_2", "facettags_2", 2)
-data_8 = ("Boundary touching", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, "celltags_2", "facettags_2", 3)
+data_1 = ("circle_in_circle", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, -1)
+data_2 = ("circle_in_circle", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, 1)
+data_3 = ("circle_in_circle", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, 2)
+data_4 = ("circle_in_circle", "disk", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, 3)
 
-testdata = [data_1, data_2, data_3, data_4,
-            data_5, data_6, data_7, data_8]
+data_5 = ("boundary_crossing_circle", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, -1)
+data_6 = ("boundary_crossing_circle", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, 1)
+data_7 = ("boundary_crossing_circle", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, 2)
+data_8 = ("boundary_crossing_circle", "disk", lambda x: x[0]**2 + (x[1] - 0.5)**2 - 0.125, 3)
+
+data_9  = ("circle_in_square", "square_quad", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, -1)
+data_10 = ("circle_in_square", "square_quad", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, 1)
+data_11 = ("circle_in_square", "square_quad", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, 2)
+data_12 = ("circle_in_square", "square_quad", lambda x: x[0, :]**2 + x[1, :]**2 - 0.125, 3)
+
+testdata = [data_1, data_2,  data_3,  data_4,
+            data_5, data_6,  data_7,  data_8,
+            data_9, data_10, data_11, data_12]
 
 parent_dir = os.path.dirname(__file__)
 
-@pytest.mark.parametrize("data_name, mesh_name, levelset, cells_benchmark_name, facets_benchmark_name, discrete_levelset_degree", testdata)
-def test_compute_meshtags(data_name, mesh_name, levelset, cells_benchmark_name, facets_benchmark_name, discrete_levelset_degree, save_as_benchmark=False):
+@pytest.mark.parametrize("data_name, mesh_name, levelset, discrete_levelset_degree", testdata)
+def test_compute_meshtags(data_name, mesh_name, levelset, discrete_levelset_degree, save_as_benchmark=False):
     mesh_path = os.path.join(parent_dir, "tests_data", "disk" + ".xdmf")
 
     if not os.path.isfile(mesh_path):
         print(f"{mesh_path} not found, we create it.")
-        create_disk(mesh_path, 0.1)
+        if mesh_name=="disk":
+            create_disk(mesh_path, 0.1)
+        if mesh_name=="square":
+            create_square(mesh_path, 0.1)
+        if mesh_name=="square_quad":
+            create_square_quad(mesh_path, 0.1)
     
-    with XDMFFile(MPI.COMM_WORLD, os.path.join(parent_dir, "tests_data", "disk.xdmf"), "r") as fi:
+    with XDMFFile(MPI.COMM_WORLD, os.path.join(parent_dir, "tests_data", mesh_name + ".xdmf"), "r") as fi:
         mesh = fi.read_mesh()
     
     if discrete_levelset_degree > 0:
@@ -57,27 +69,28 @@ def test_compute_meshtags(data_name, mesh_name, levelset, cells_benchmark_name, 
     # To save benchmark
     if save_as_benchmark:
         cells_benchmark = np.vstack([cells_tags.indices, cells_tags.values])
-        np.savetxt(os.path.join(parent_dir, "tests_data", cells_benchmark_name +".csv"), cells_benchmark, delimiter=" ", newline="\n")
+        np.savetxt(os.path.join(parent_dir, "tests_data", data_name + "_cells_tags.csv"), cells_benchmark, delimiter=" ", newline="\n")
 
-        fig = plt.figure()
-        ax = fig.subplots()
-        plot_mesh_tags(mesh, cells_tags, ax, expression_levelset=levelset)
-        plt.savefig(os.path.join(parent_dir, "tests_data", cells_benchmark_name +".png"), dpi=500, bbox_inches="tight")
+        if mesh.topology.cell_name() == "triangle":
+            fig = plt.figure()
+            ax = fig.subplots()
+            plot_mesh_tags(mesh, cells_tags, ax, expression_levelset=levelset)
+            plt.savefig(os.path.join(parent_dir, "tests_data", data_name + "_cells_tags.png"), dpi=500, bbox_inches="tight")
+            fig = plt.figure()
+            ax = fig.subplots()
+            plot_mesh_tags(mesh, facets_tags, ax, expression_levelset=levelset, linewidth=1.5)
+            plt.savefig(os.path.join(parent_dir, "tests_data", data_name + "_facets_tags.png"), dpi=500, bbox_inches="tight")
 
         facets_benchmark = np.vstack([facets_tags.indices, facets_tags.values])
-        np.savetxt(os.path.join(parent_dir, "tests_data", facets_benchmark_name + ".csv"), facets_benchmark, delimiter=" ", newline="\n")
+        np.savetxt(os.path.join(parent_dir, "tests_data", data_name + "_facets_tags.csv"), facets_benchmark, delimiter=" ", newline="\n")
 
-        fig = plt.figure()
-        ax = fig.subplots()
-        plot_mesh_tags(mesh, facets_tags, ax, expression_levelset=levelset, linewidth=1.5)
-        plt.savefig(os.path.join(parent_dir, "tests_data", facets_benchmark_name +".png"), dpi=500, bbox_inches="tight")
     else:
         try:
-            cells_benchmark = np.loadtxt(os.path.join(parent_dir, "tests_data", cells_benchmark_name + ".csv"), delimiter=" ")
+            cells_benchmark = np.loadtxt(os.path.join(parent_dir, "tests_data", data_name + "_cells_tags.csv"), delimiter=" ")
         except FileNotFoundError:
             raise FileNotFoundError("{cells_benchmark_name} not found, have you generated the benchmark ?")
         try:
-            facets_benchmark = np.loadtxt(os.path.join(parent_dir, "tests_data", facets_benchmark_name + ".csv"), delimiter=" ")
+            facets_benchmark = np.loadtxt(os.path.join(parent_dir, "tests_data", data_name + "_facets_tags.csv"), delimiter=" ")
         except FileNotFoundError:
             raise FileNotFoundError("{facets_benchmark_name} not found, have you generated the benchmark ?")
 
@@ -89,5 +102,5 @@ def test_compute_meshtags(data_name, mesh_name, levelset, cells_benchmark_name, 
 
 
 if __name__=="__main__":
-    test_data = data_1
-    test_compute_meshtags(test_data[0], test_data[1], test_data[2], test_data[3], test_data[4], test_data[5], save_as_benchmark=True)
+    for test_data in [data_1, data_5, data_9]:
+        test_compute_meshtags(test_data[0], test_data[1], test_data[2], test_data[3], save_as_benchmark=True)
