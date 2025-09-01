9d31ed37ad9ed215bb8b9abe11e8c33266692296
rbulle <rbulle@tutanota.com>
2025-09-01 15:10:01 +0200

    Complete the interface elasticity test case with an ellipse interface for which inside and outside cells touch.

--- test-cases/interface-elasticity/data.py
+++ test-cases/interface-elasticity/data.py
@@ -1,10 +1,6 @@
 import numpy as np
 import ufl
 
-DISK_RADIUS = np.pi/10.
-# Disk center
-X0, Y0 = [0.5, 0.5]
-
 # Lamé coefficients
 def lmbda(E, nu):
     return E * nu/(1.0+nu)/(1.-2.*nu)
@@ -12,12 +8,12 @@ def mu(E, nu):
     return E/2.0/(1.0+nu)
 
 # Material parameters inside the disk
-E_in = 0.001
+E_in = 1.
 nu_in = 0.3
 lmbda_in = lmbda(E_in, nu_in)
 mu_in = mu(E_in, nu_in)
 # Material parameters outside the disk
-E_out = 1.0
+E_out = 0.001
 nu_out = 0.3
 lmbda_out = lmbda(E_out, nu_out)
 mu_out = mu(E_out, nu_out)
@@ -31,10 +27,8 @@ def sigma_in(u):
 def sigma_out(u):
     return lmbda_out * ufl.nabla_div(u)*ufl.Identity(len(u)) + 2.0 * mu_out * epsilon(u)
 
-def levelset(x):
-    return (x[0] - X0)**2 + (x[1] - Y0)**2 - DISK_RADIUS**2
+def levelset_1(x):
+    return 0.65 - (x[0]**2 + (0.6 * x[1])**2)
 
-def neumann(x):
-    vals_x = 1.e-1 * np.ones_like(x[0])
-    vals = np.vstack([vals_x, np.zeros_like(x[1])])
-    return vals
+def levelset_2(x):
+    return 1. - ((x[0])**2 + (x[1])**2)
--- test-cases/interface-elasticity/main.py
+++ test-cases/interface-elasticity/main.py
@@ -53,13 +53,7 @@ def save_function(fct, file_name):
             of.write_mesh(mesh)
             of.write_function(fct)
 
-from data import levelset, sigma_in, sigma_out, epsilon, neumann
-
-try:
-    from data import detection_levelset
-except ImportError:
-    print("detection_levelset not found in data, use levelset instead.")
-    detection_levelset = levelset
+from data import levelset_1, levelset_2, sigma_in, sigma_out, epsilon
 
 with open(parameters_path, "rb") as f:
     parameters = yaml.safe_load(f)
@@ -69,33 +63,54 @@ mesh_size                 = parameters["initial_mesh_size"]
 primal_degree             = parameters["primal_degree"]
 flux_degree               = parameters["flux_degree"]
 auxiliary_degree          = parameters["auxiliary_degree"]
+levelset_number           = parameters["levelset_number"]
 levelset_degree           = parameters["levelset_degree"]
 detection_degree          = parameters["boundary_detection_degree"]
 bbox                      = parameters["bbox"]
 penalization_coefficient  = parameters["penalization_coefficient"]
 stabilization_coefficient = parameters["stabilization_coefficient"]
+cell_type                 = parameters["cell_type"]
+
+if levelset_number == 1:
+    try:
+        from data import detection_levelset_1
+    except ImportError:
+        print("detection_levelset_1 not found in data, use levelset instead.")
+        detection_levelset = levelset_1
+    levelset = levelset_1
+elif levelset_number == 2:
+    try:
+        from data import detection_levelset_2
+    except ImportError:
+        print("detection_levelset_2 not found in data, use levelset instead.")
+        detection_levelset = levelset_2
+    levelset = levelset_2
+else:
+    raise ValueError("Parameter error levelset_number can only be 1 or 2.")
 
 # Create the background mesh
-nx = int(np.abs(bbox[0][1] - bbox[0][0]) * np.sqrt(2.) / mesh_size)
-ny = int(np.abs(bbox[1][1] - bbox[1][0]) * np.sqrt(2.) / mesh_size)
+nx = int(np.abs(bbox[0][1] - bbox[0][0]) / mesh_size)
+ny = int(np.abs(bbox[1][1] - bbox[1][0]) / mesh_size)
 # Quads cells
-cell_type = dfx.cpp.mesh.CellType.triangle
-# cell_type = dfx.cpp.mesh.CellType.quadrilateral
+if cell_type == "triangle":
+    cell_type = dfx.cpp.mesh.CellType.triangle
+elif cell_type == "quadrilateral":
+    cell_type = dfx.cpp.mesh.CellType.quadrilateral
+else:
+    raise ValueError("Parameter error cell_type can only be 'triangle' or 'quadrilateral'.")
 mesh = dfx.mesh.create_rectangle(MPI.COMM_WORLD, np.asarray(bbox).T, [nx, ny], cell_type)
 
 cells_tags, facets_tags, _, d_from_inside, d_from_outside, _ = compute_tags_measures(mesh, levelset, detection_degree, box_mode=True)
 
-"""
-ax = fig.subplots()
 fig = plt.figure()
-plot_mesh_tags(mesh, facets_tags, ax, linewidth=0.2)
-plt.savefig("facets_tags.png", dpi=500, bbox_inches="tight")
+ax = fig.subplots()
+plot_mesh_tags(mesh, facets_tags, ax, linewidth=1.5)
+plt.savefig(os.path.join(output_dir, "facets_tags.png"), dpi=500, bbox_inches="tight")
 
 fig = plt.figure()
 ax = fig.subplots()
-plot_mesh_tags(mesh, cells_tags, ax, linewidth=0.2)
-plt.savefig("cells_tags.png", dpi=500, bbox_inches="tight")
-"""
+plot_mesh_tags(mesh, cells_tags, ax, linewidth=1.)
+plt.savefig(os.path.join(output_dir, "cells_tags.png"), dpi=500, bbox_inches="tight")
 
 gdim = mesh.geometry.dim
 cell_name = mesh.topology.cell_name()
@@ -119,8 +134,8 @@ primal_space = dfx.fem.functionspace(mesh, primal_element)
 mixed_space  = dfx.fem.functionspace(mesh, mixd_element)
 
 def point_source(x):
-    return np.logical_and(np.isclose(x[0, :], 1.0),
-                            np.abs(x[1, :] - 0.5 * np.ones_like(x[1, :])) <= 1.0/ny)
+    return np.logical_and(np.isclose(x[0, :], 1.5),
+                            np.abs(x[1, :] - 0.75 * np.ones_like(x[1, :])) <= 2.0/ny)
 
 # Defines mapping between matrices and functions
 primal_space_y = primal_space.sub(1)
@@ -150,38 +165,22 @@ dx = ufl.Measure("dx", domain=mesh, subdomain_data=cells_tags)
 dS = ufl.Measure("dS", domain=mesh, subdomain_data=facets_tags)
 
 def boundary_dbc(x):
-    left  = np.isclose(x[0], 0.).astype(bool)
+    left  = np.isclose(x[0], -1.5).astype(bool)
     return left
-
-def boundary_nbc(x):
-    right = np.isclose(x[0], 1.).astype(bool)
-    return right
 
 boundary_dbc_facets = dfx.mesh.locate_entities_boundary(mesh,
                                                         gdim - 1,
                                                         boundary_dbc)
-boundary_nbc_facets = dfx.mesh.locate_entities_boundary(mesh,
-                                                        gdim - 1,
-                                                        boundary_nbc)
-
-boundary_indices = np.hstack([boundary_dbc_facets, boundary_nbc_facets])
-sorted_indices = np.argsort(boundary_indices)
-dbc_markers = np.ones_like(boundary_dbc_facets).astype(np.int32)
-nbc_markers = 2 * np.ones_like(boundary_nbc_facets).astype(np.int32)
-boundary_markers = np.hstack([dbc_markers, nbc_markers])
-boundary_tags = dfx.mesh.meshtags(mesh, gdim - 1, boundary_indices[sorted_indices], boundary_markers[sorted_indices])
-
-ds = ufl.Measure("ds", domain=mesh, subdomain_data=boundary_tags)
 
 # Create a FE function from outer space
 u_dbc = dfx.fem.Function(mixed_space)
-u_nbc = dfx.fem.Function(mixed_space)
-_, u_dbc_out, _, _, _ = u_dbc.split()
-_, u_nbc_out, _, _, _ = u_nbc.split()
-u_nbc_out.interpolate(neumann)
+u_dbc_in, u_dbc_out, _, _, _ = u_dbc.split()
 
-bc_dofs = dfx.fem.locate_dofs_topological(mixed_space.sub(1), gdim - 1, boundary_dbc_facets)
-bc = dfx.fem.dirichletbc(u_dbc_out, bc_dofs)
+bc_in_dofs = dfx.fem.locate_dofs_topological(mixed_space.sub(0), gdim - 1, boundary_dbc_facets)
+bc_out_dofs = dfx.fem.locate_dofs_topological(mixed_space.sub(1), gdim - 1, boundary_dbc_facets)
+dbc_in  = dfx.fem.dirichletbc(u_dbc_in,  bc_in_dofs)
+dbc_out = dfx.fem.dirichletbc(u_dbc_out, bc_out_dofs)
+bcs = [dbc_in, dbc_out]
 
 n = ufl.FacetNormal(mesh)
 boundary_in = ufl.inner(ufl.dot(y_in, n), v_in)
@@ -193,7 +192,7 @@ stiffness_in  = ufl.inner(sigma_in(u_in),   epsilon(v_in))
 stiffness_out = ufl.inner(sigma_out(u_out), epsilon(v_out))
 
 penalization = penalization_coefficient * \
-            ( ufl.inner(y_in + sigma_in(u_in), z_in + sigma_in(v_in)) / 0.001**2 \
+            ( ufl.inner(y_in + sigma_in(u_in), z_in + sigma_in(v_in))\
             + ufl.inner(y_out + sigma_out(u_out), z_out + sigma_out(v_out)) \
 + h_T**(-2) * ufl.inner(ufl.dot(y_in, ufl.grad(phi_h)) - ufl.dot(y_out, ufl.grad(phi_h)), ufl.dot(z_in, ufl.grad(phi_h)) - ufl.dot(z_out, ufl.grad(phi_h))) \
 + h_T**(-2) * ufl.inner(u_in - u_out + h_T**(-1) * p * phi_h, v_in - v_out + h_T**(-1) * q * phi_h))
@@ -201,10 +200,10 @@ penalization = penalization_coefficient * \
 stabilization_facets_in = stabilization_coefficient * \
                     ufl.avg(h_T) * \
                     ufl.inner(ufl.jump(sigma_in(u_in), n),
-                              ufl.jump(sigma_in(v_in), n)) / 0.001**2
+                              ufl.jump(sigma_in(v_in), n))
 
 stabilization_cells_in = stabilization_coefficient * \
-                    ufl.inner(ufl.div(y_in), ufl.div(z_in)) / 0.001**2
+                    ufl.inner(ufl.div(y_in), ufl.div(z_in))
 
 stabilization_cells_out = stabilization_coefficient * \
                     ufl.inner(ufl.div(y_out), ufl.div(z_out))
@@ -214,8 +213,8 @@ stabilization_facets_out = stabilization_coefficient * \
                     ufl.inner(ufl.jump(sigma_out(u_out), n),
                               ufl.jump(sigma_out(v_out), n))
 
-a =  stiffness_in             * (dx(1) + dx(2)) \
-   + stiffness_out            * (dx(2) + dx(3)) \
+a =  stiffness_in             * dx((1,2)) \
+   + stiffness_out            * dx((2,3)) \
    + penalization             * dx(2) \
    + stabilization_facets_in  * dS(3) \
    + stabilization_facets_out * dS(4) \
@@ -228,9 +227,9 @@ boundary_out_int = boundary_out * d_from_outside
 bilinear_form = dfx.fem.form(a)
 bdy_in_form = dfx.fem.form(boundary_in_int)
 bdy_out_form = dfx.fem.form(boundary_out_int)
-A_temp    = assemble_matrix(bilinear_form, bcs=[bc])
-A_bdy_in  = assemble_matrix(bdy_in_form,   bcs=[bc])
-A_bdy_out = assemble_matrix(bdy_out_form,  bcs=[bc])
+A_temp    = assemble_matrix(bilinear_form, bcs=bcs)
+A_bdy_in  = assemble_matrix(bdy_in_form,   bcs=bcs)
+A_bdy_out = assemble_matrix(bdy_out_form,  bcs=bcs)
 A_temp.assemble()
 A_bdy_in.assemble()
 A_bdy_out.assemble()
@@ -241,7 +240,8 @@ ksp.setType("preonly")
 solver = ksp.create(MPI.COMM_WORLD)
 solver.setFromOptions()
 solver.setOperators(A)
-# Configure MUMPS to handle pressure nullspace
+
+# Configure MUMPS to handle nullspace
 pc = solver.getPC()
 pc.setType("lu")
 pc.setFactorSolverType("mumps")
@@ -250,14 +250,14 @@ pc.getFactorMatrix().setMumpsIcntl(icntl=24, ival=1)
 pc.getFactorMatrix().setMumpsIcntl(icntl=25, ival=0)
 
 stabilization_rhs_in = stabilization_coefficient * \
-                    (ufl.inner(f_h, ufl.div(z_in))) / 0.001
+                    (ufl.inner(f_h, ufl.div(z_in)))
 stabilization_rhs_out = stabilization_coefficient * \
                     (ufl.inner(f_h, ufl.div(z_out)))
 rhs_in  = ufl.inner(f_h, v_in)
 rhs_out = ufl.inner(f_h, v_out)
 
-L = rhs_in                * (dx(1) + dx(2)) \
-  + rhs_out               * (dx(2) + dx(3)) \
+L = rhs_in                * dx((1,2)) \
+  + rhs_out               * dx((2,3)) \
   - stabilization_rhs_in  * dx(2) \
   - stabilization_rhs_out * dx(2)
 
@@ -285,8 +285,10 @@ solution_uh = solution_uh.collapse()
 
 mesh.topology.create_connectivity(gdim, gdim)
 dofs_cut = dfx.fem.locate_dofs_topological(mixed_space.sub(0), gdim, cells_tags.find(2))
+dofs_direct_interface = dfx.fem.locate_dofs_topological(mixed_space.sub(0), gdim - 1, facets_tags.find(6))
+dofs_to_turn_off = np.union1d(dofs_cut, dofs_direct_interface)
 
-solution_uh_in.x.array[dofs_cut] = 0.
+solution_uh_in.x.array[dofs_to_turn_off] = 0.
 solution_uh_in = solution_uh_in.collapse()
 solution_uh_out = solution_uh_out.collapse()
 solution_uh.x.array[:] = solution_uh_in.x.array[:] + solution_uh_out.x.array[:]
