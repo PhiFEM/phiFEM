a09400d02ee0a0bf68f2aeb1ed4e743280ffcef2
Raphaël Bulle <rbulle@tutanota.com>
2026-08-24 17:00:03 +0200

    Improve utils_test.

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,4 +1,5 @@
 import dolfinx as dfx
+import numpy as np
 from basix.ufl import element
 from dolfinx.io import XDMFFile
 
@@ -12,24 +13,32 @@ def save_tags(mesh, file_path, mesh_tags):
         mesh_tags: the MeshTags object to save.
     """
 
-    try:
-        mesh.topology.dim == mesh_tags.dim
-    except ValueError:
-        print("The Mesh and MeshTags objects must have the same topological dimension.")
+    if mesh_tags.dim == mesh.topology.dim:
+        plot_mesh = mesh
+    elif mesh_tags.dim == mesh.topology.dim - 1:
+        all_facets = dfx.mesh.locate_entities(
+            mesh, mesh_tags.dim, lambda x: np.ones_like(x[0]).astype(np.int32)
+        )
+        plot_mesh = dfx.mesh.create_submesh(mesh, mesh_tags.dim, all_facets)[0]
+    else:
+        print(
+            "mesh_tags argument must be of dimension at least 1 less that of mesh topology dimension."
+        )
+        raise ValueError
 
-    cell_name = mesh.topology.cell_name()
+    cell_name = plot_mesh.topology.cell_name()
     dg0_element = element("DG", cell_name, 0)
-    dg0_space = dfx.fem.functionspace(mesh, dg0_element)
+    dg0_space = dfx.fem.functionspace(plot_mesh, dg0_element)
     dg0_mesh_tags = dfx.fem.Function(dg0_space)
 
-    dg0_mesh_tags.x.array[:] = mesh_tags.values
+    dg0_mesh_tags.x.array[mesh_tags.indices] = mesh_tags.values
 
     with XDMFFile(
-        mesh.comm,
+        plot_mesh.comm,
         file_path,
         "w",
     ) as of:
-        of.write_mesh(mesh)
+        of.write_mesh(plot_mesh)
         of.write_function(dg0_mesh_tags)
 
 
